b3065341b0c4333c2c897f9e04ced87c7c5f6b49
ArseniiMakhov <arseniimakhov@gmail.com>
2024-01-16 21:33:10 +0300

    fixed

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
+
 import Footer from '../Footer';
 import TaskList from '../TaskList';
 import NewTaskForm from '../NewTaskForm';
